c999f4da23dae7ec2baf89f9dc1594e318789514
Azdzirt <51914013+Azdzirt@users.noreply.github.com>
2019-07-26 12:30:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+a="введите число";
+b=2;
+alert((prompt(a))*(b));
+
+
+
+var age = "Ваш возраст?";
+alert(prompt(age));
+
+var name = "Ваше имя?";
+alert(prompt(name));
+
+var name2 = "Гэндальф это ты???";
+alert(name2);
+
+var age2 = "старик 333 года! ты серьезно?!?!?";
+alert(age2);
+
+var age3 = "ТЫ НЕ ПРОЙДЕШЬ!!!!!!!";
+alert(age3);
+
+var bool = true;
+alert(typeof true);
+
+var nol = null;
+alert(typeof null);
+
+var undefined = undefined;
+alert(typeof undefined);
+
+
+
